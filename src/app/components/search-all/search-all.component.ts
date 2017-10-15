4a4d7018ce7f2c7ac93d34232c52d32b7ed724cd
Joshua Maynard <jmaynard100@gmail.com>
2017-10-15 21:40:40 +1100

    Search is done

--- src/app/components/search-all/search-all.component.ts
+++ src/app/components/search-all/search-all.component.ts
@@ -1,6 +1,6 @@
 import { UserDataService } from './../../services/user-data.service';
 import { ApiService } from './../../services/api.service';
-import { Journal } from './../../journal.model';
+import { Journal, JournalEntry } from './../../journal.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-all.component.css']
 })
 export class SearchAllComponent implements OnInit {
-  public journals: Array<any>;
+  public journals: Array <any>;
+  public visibleJournals: Array <any>;
+  public journal: Journal;
+  public visibleEntries: Array <any>;
   public searchParam = '';
   constructor (private userData: UserDataService) {
   }
@@ -20,21 +23,54 @@ export class SearchAllComponent implements OnInit {
 
   journalContains(journal: Journal): Boolean {
     var contains = false;
+    this.visibleEntries = [];
+    this.journal = journal;
     journal.journalEntry.forEach(entry => {
+      this.visibleEntries.push(entry);
+      console.log(this.visibleEntries);
       if (entry.content.indexOf(this.searchParam) !== -1 || entry.title.indexOf(this.searchParam) !== -1) {
         contains = true;
-      } 
+      }
     });
+    return contains;
+  }
+
+  entryContains(entry: JournalEntry): Boolean {
+    var contains = false;
+    if (entry.content.indexOf(this.searchParam) !== -1 || entry.title.indexOf(this.searchParam) !== -1) {
+      contains = true;
+    }
     return contains;
   }
 
   searchJournals(searchValue: string): void {
     this.searchParam = searchValue;
-    this.filterEntries();
+    this.filterJournals();
   }
 
-  filterEntries(): void {
+  filterJournals(): void {
+    var contains = false;
+    this.visibleJournals = [];
+    this.journals.forEach(journal => {
+      journal.journalEntry.forEach(entry => {
+        if (entry.content.indexOf(this.searchParam) !== -1 || entry.title.indexOf(this.searchParam) !== -1) {
+          contains = true;
+        }
+      });
+      if (contains) {
+        this.visibleJournals.push(journal);
+        console.log(this.visibleJournals);
+      }
+    });
+  }
 
+  setJournal(journal: Journal) {
+    this.visibleEntries = [];
+    this.journal = journal;
+    journal.journalEntry.forEach(entry => {
+      this.visibleEntries.push(entry);
+      console.log(this.visibleEntries);
+    });
   }
 
 }
