6c6b956e26a573b7fa24a9d2f1e86da1f6bb3cf1
Joshua Maynard <jmaynard100@gmail.com>
2017-10-22 11:45:05 +1100

    Documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { UserDataService } from './services/user-data.service';
 import { LoginComponent } from './components/login/login.component';
 import { Component } from '@angular/core';
 
+/**
+ * 
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
   constructor ( public userData: UserDataService, private router: Router, public alerts: AlertService) {  }
